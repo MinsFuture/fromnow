e5067680db96eddd648378004aa6baa4a9d27596
이민혁 <helloaway213@gmail.com>
2024-07-15 20:37:10 +0900

    Feat : Oauth2 로그인 및 회원가입 로직 구현

--- src/main/java/com/knu/fromnow/api/auth/oauth2/controller/Oauth2Api.java
+++ src/main/java/com/knu/fromnow/api/auth/oauth2/controller/Oauth2Api.java
@@ -2,6 +2,7 @@ package com.knu.fromnow.api.auth.oauth2.controller;
 
 import com.fasterxml.jackson.core.JsonProcessingException;
 import com.knu.fromnow.api.global.spec.ApiBasicResponse;
+import io.swagger.v3.oas.annotations.Operation;
 import io.swagger.v3.oas.annotations.Parameter;
 import io.swagger.v3.oas.annotations.tags.Tag;
 import net.minidev.json.parser.ParseException;
@@ -9,6 +10,8 @@ import org.springframework.http.ResponseEntity;
 
 @Tag(name = "회원", description = "회원 관련 api")
 public interface Oauth2Api {
+
+    @Operation(summary = "Google 로그인 로직", description = "id_token을 쿼리파라미터 형식으로 보내주면 됨")
     ResponseEntity<ApiBasicResponse> oauth2Google(
             @Parameter(description = "id_token", required = true) String id_token) throws ParseException, JsonProcessingException;
 }
