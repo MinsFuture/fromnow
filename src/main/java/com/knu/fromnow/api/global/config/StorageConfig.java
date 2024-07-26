3dfb39d515de72cf3e8753980def685c49ce713c
이민혁 <helloaway213@gmail.com>
2024-07-26 22:41:14 +0900

    Chore : DockerFile 수정

--- src/main/java/com/knu/fromnow/api/global/config/StorageConfig.java
+++ src/main/java/com/knu/fromnow/api/global/config/StorageConfig.java
@@ -7,6 +7,7 @@ import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.core.io.ClassPathResource;
 
+import java.io.FileInputStream;
 import java.io.IOException;
 
 @Configuration
@@ -15,14 +16,19 @@ public class StorageConfig {
     @Bean
     public Storage storage() throws IOException {
 
-        ClassPathResource resource = new ClassPathResource("sunny-wavelet-429609-t9-5d820b98637e.json");
-        GoogleCredentials credentials = GoogleCredentials.fromStream(resource.getInputStream());
+        // 파일 시스템 경로에서 직접 파일을 읽기
+        FileInputStream inputStream = new FileInputStream("/home/helloaway214/bucket/sunny-wavelet-429609-t9-5d820b98637e.json");
+
+        // GoogleCredentials 객체 생성
+        GoogleCredentials credentials = GoogleCredentials.fromStream(inputStream);
+
+        // 프로젝트 ID 설정
         String projectId = "sunny-wavelet-429609-t9";
 
+        // Storage 객체 생성 및 반환
         return StorageOptions.newBuilder()
                 .setProjectId(projectId)
                 .setCredentials(credentials)
                 .build()
                 .getService();
-    }
 }
