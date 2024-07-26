016bb5a26eae74e15b41009941cf24903322f324
이민혁 <helloaway213@gmail.com>
2024-07-26 23:09:45 +0900

    Chore : DockerFile 수정

--- src/main/java/com/knu/fromnow/api/global/config/StorageConfig.java
+++ src/main/java/com/knu/fromnow/api/global/config/StorageConfig.java
@@ -7,30 +7,28 @@ import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.core.io.ClassPathResource;
 
-import java.io.File;
 import java.io.FileInputStream;
+import java.io.FileNotFoundException;
 import java.io.IOException;
-import java.nio.file.Path;
-import java.nio.file.Paths;
 
 @Configuration
 public class StorageConfig {
 
     @Bean
     public Storage storage() throws IOException {
+        try {
+            FileInputStream inputStream = new FileInputStream("/config/sunny-wavelet-429609-t9-5d820b98637e.json");
+            GoogleCredentials credentials = GoogleCredentials.fromStream(inputStream);
+            String projectId = "sunny-wavelet-429609-t9";
 
-        Path path = Paths.get("/home/helloaway214/bucket/sunny-wavelet-429609-t9-5d820b98637e.json");
-        FileInputStream inputStream = new FileInputStream(path.toAbsolutePath().toString());
-
-        // GoogleCredentials 객체 생성
-        GoogleCredentials credentials = GoogleCredentials.fromStream(inputStream);
-
-        String projectId = "sunny-wavelet-429609-t9";
-
-        return StorageOptions.newBuilder()
-                .setProjectId(projectId)
-                .setCredentials(credentials)
-                .build()
-                .getService();
-    }
+            return StorageOptions.newBuilder()
+                    .setProjectId(projectId)
+                    .setCredentials(credentials)
+                    .build()
+                    .getService();
+        } catch (FileNotFoundException e) {
+            throw new RuntimeException("File not found at path: /config/sunny-wavelet-429609-t9-5d820b98637e.json", e);
+        } catch (IOException e) {
+            throw new RuntimeException("Error reading file at path: /config/sunny-wavelet-429609-t9-5d820b98637e.json", e);
+        }
 }
