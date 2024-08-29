1ee9f179adbd04740da608a00addc22f0185e613
이민혁 <helloaway213@gmail.com>
2024-08-29 18:36:05 +0900

    Chore : 카카오 로그인 중복회원 검사 로직 추가

--- src/main/java/com/knu/fromnow/api/domain/member/entity/Role.java
+++ src/main/java/com/knu/fromnow/api/domain/member/entity/Role.java
@@ -1,5 +1,5 @@
 package com.knu.fromnow.api.domain.member.entity;
 
 public enum Role {
-    ROLE_GOOGLE_USER, ROLE_KAKAO_USER, ROLE_PREMIUM_USER, ROLE_ADMIN
+    ROLE_GOOGLE_USER, ROLE_KAKAO_USER, ROLE_NEW_USER, ROLE_PREMIUM_USER, ROLE_ADMIN
 }
