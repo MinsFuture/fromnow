b48420d34732bc01a4279472d5bdd15bc48a55f8
이민혁 <helloaway213@gmail.com>
2024-09-25 19:05:32 +0900

    Chore : profileName validation 수정

--- src/main/java/com/knu/fromnow/api/domain/member/dto/request/CreateMemberDto.java
+++ src/main/java/com/knu/fromnow/api/domain/member/dto/request/CreateMemberDto.java
@@ -9,7 +9,7 @@ import lombok.Getter;
 @Getter
 public class CreateMemberDto {
     @NotNull(message = "ProfileName은 필드가 없거나 요청 스펙이 잘못 되었습니다")
-    @Size(min = 4, max = 12, message = "ProfileName은 4글자에서 12글자 사이여야 합니다")
-    @Pattern(regexp = "^[a-zA-Z0-9]*$", message = "ProfileName은 영문/숫자만 가능합니다")
+    @Size(min = 2, max = 10, message = "ProfileName은 2글자에서 10글자 사이여야 합니다")
+    @Pattern(regexp = "^[a-zA-Z0-9가-힣]*$", message = "ProfileName은 한글, 영문, 숫자만 가능합니다")
     private String profileName;
 }
