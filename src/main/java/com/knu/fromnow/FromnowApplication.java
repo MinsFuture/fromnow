7897860a82bd244a187c7ddb8be5abf9afebfd4e
이민혁 <helloaway213@gmail.com>
2024-12-02 19:13:17 +0900

    Chore : yml에 DB 서버 시간 동기화

--- src/main/java/com/knu/fromnow/FromnowApplication.java
+++ src/main/java/com/knu/fromnow/FromnowApplication.java
@@ -1,6 +1,5 @@
 package com.knu.fromnow;
 
-import jakarta.annotation.PostConstruct;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.data.jpa.repository.config.EnableJpaAuditing;
@@ -13,12 +12,8 @@ import java.util.TimeZone;
 @EnableJpaAuditing
 public class FromnowApplication {
 
-	@PostConstruct
-	public void setTimeZone(){
-		TimeZone.setDefault(TimeZone.getTimeZone("Asia/Seoul"));
-	}
-
 	public static void main(String[] args) {
+		TimeZone.setDefault(TimeZone.getTimeZone("Asia/Seoul"));
 		SpringApplication.run(FromnowApplication.class, args);
 	}
 
